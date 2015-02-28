8cb3868015547e2c460558f5e0790539d98f1634
Gabor Szabo <szabogabi@gmail.com>
2015-02-28 15:38:08 +0100

    offcanvas bug fix

--- base.php
+++ base.php
@@ -18,7 +18,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
     </div>
   <![endif]-->
-
+    
+  <div class="container"> 
   <?php
     do_action('get_header');
     get_template_part('templates/header');
@@ -28,7 +29,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <div class="subcsusza">
       <div class="subcsusza__side">
         <div class="side__inner">
-        <?php //global $response; ?>
           <a href="#" class="btn--sideclose"><i class="ion ion-close"></i></a>
           <?php get_template_part('/templates/jelentkezes','form'); ?>
         </div>
@@ -48,13 +48,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </main><!-- /.main -->
 
       <?php get_template_part('templates/footer'); ?>
-      </div>
+    </div>
 
-    </div><!-- /.subcsusza -->
+    </div> <!--/.subcsusza -->
 
   </div><!-- /.document -->
 
-  <?php wp_footer(); ?>
+    </div><!-- /.container -->
 
+
+  <?php wp_footer(); ?>
+  <script src="//szabogabi.com:35729/livereload.js"></script>
 </body>
 </html>
